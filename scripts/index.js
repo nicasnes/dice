9d6292715a3be9fcb943e77d17a8e0cbf87ee1f1
nicasnes <31024845+nicasnes@users.noreply.github.com>
2022-07-31 21:38:32 -0400

    Add advantage

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
-const roll = (max) => { 
-  return 1 + Math.floor(Math.random() * max)
+const roll = (max, mod) => { 
+  return (1 + Math.floor(Math.random() * max)) + mod
 }
 
 $(document).ready(() => { 
@@ -8,15 +8,55 @@ $(document).ready(() => {
   });
   const submit = $("input[type=submit]")
   submit.click(() => { 
+    $("#resultText").css("color", "")
+    $("#advantageText").remove()
     const amountOfDice = parseInt($("#amount").val())
     const typeOfDie = parseInt($("#type").val())
     const modifier = parseInt($("#mod").val()) || 0
+    const isAdvantage = $("#advantage").val() === 'yes'
     const result = $("#resultText")
     result.text("")
-    console.log(amountOfDice, typeOfDie, modifier)
-    for (let i = 1; i < amountOfDice; i++) { 
-      result.text(`${result.text()}  ${roll(typeOfDie) + modifier} \t|\t`)
+
+    if (isAdvantage) { 
+      const clone = result.clone()
+      clone.text("")
+      clone.attr('id', 'advantageText')
+      $("#results").append(clone)
+      $("#resultText").css("color", "red")
     }
-    result.text(`${result.text()}  ${roll(typeOfDie) + modifier}`)
+    const advantageText = $("#advantageText")
+
+    const modifyDOMAdvantage = (r1, r2, isLast) => { 
+      r1 = roll(typeOfDie, modifier)
+      r2 = roll(typeOfDie, modifier)
+      if (r2 > r1) { 
+        advantageText.text(`${advantageText.text()} ${r2} \t|\t`)
+      } else { 
+        advantageText.text(`${advantageText.text()} ${r1} \t|\t`)
+        r1 = r2
+      }
+      result.text(`${result.text()}${r1} \t|\t`)
+      if(isLast) { 
+        const text = result.text()
+        result.text(text.substring(0,text.length-4))
+        
+        const advText = advantageText.text()
+        advantageText.text(advText.substring(0,advText.length-4))
+      }
+    }
+    for (let i = 1; i < amountOfDice; i++) { 
+      let rollValue = roll(typeOfDie, modifier)
+      if (isAdvantage) { 
+        modifyDOMAdvantage(rollValue, roll(typeOfDie, modifier), false)
+      } else { 
+        result.text(`${result.text()}${rollValue} \t|\t`)
+      }
+    }
+    if (isAdvantage) { 
+      modifyDOMAdvantage(roll(typeOfDie, modifier), roll(typeOfDie, modifier), true)
+    } else { 
+    result.text(`${result.text()}  ${roll(typeOfDie, modifier)}`)
+    }
+
   })
 })
